41bae0daa0026baee23c2baa41a7ebf89f806ecf
FakeEmperor <fake_empire@mail.ru>
2017-06-05 13:16:04 +0300

    Updated test client

--- test_send.py
+++ test_send.py
@@ -1,20 +1,66 @@
 # send - host / port
-import json
+import json	
 import socket
+import sys
+
+
+class Unbuffered(object):
+
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def writelines(self, datas):
+        self.stream.writelines(datas)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
+
+
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+getch = _find_getch()
 
 HOST = "127.0.0.1"
 PORT = 10000
+
 
 def send_json(host, port, jsdata):
     raw = json.dumps(jsdata).encode('utf-8')
     l = len(raw).to_bytes(4, 'big')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-
+    print("Sending: {}{}".format(l, raw))
     s.send(l)
     s.send(raw)
-    
-    while(True):
+    print("Receiving...")
+    while True:
         try:
             msg_part = s.recv(4)
             print(msg_part.decode('utf-8'), end='', flush=True)
@@ -25,10 +71,16 @@ def send_json(host, port, jsdata):
     s.close()
 
 DATA = {
-   "algorithm": "crc32",
-   "flags": 0x03,
-   "message_direction": 0x01,
+    "algorithm": "crc32",
+    "flags": 0x03,
+    "message_direction": 0x01,
     "input": "Hello!!"
 }
 
-send_json(HOST, PORT, DATA)
+if __name__ == "__main__":
+    print("Starting tests...")
+    try:
+        send_json(HOST, PORT, DATA)
+    except KeyboardInterrupt:
+        print("Finishing...")
+        exit()
